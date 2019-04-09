87878e8611e89b9da56682aa9344867b98d140cc
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2019-04-09 14:56:51 +0300

    Add initial config for socket.io

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
-
 const app = express();
+
+const http = require('http');
+const server = http.Server(app);
+
+const socketIO = require('socket.io');
+const io = socketIO(server);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`App is listening on ${port}`);
+});
+
+io.on('connection', (socket) => {
+  console.log('New user has joined!');
 })
+
